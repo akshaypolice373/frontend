b0cdd359aa8efa1778b713ea8bd7ca177f687aa6
Akshay Kumar Police Patil <akshaypolice373@gmail.com>
2021-05-25 22:08:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ListStudentComponent from "./ListStudentComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <ListStudentComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/StudentServices.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const STUDENT_API_BASE_URL="http://localhoxst:8081/api/students";
+
+class StudentServices {
+      
+    getStudents() {
+        return axios.get(STUDENT_API_BASE_URL);
+    }
+}
+
+export default new StudentServices();
